2190fbe56657ce5a3d33c48ddaba5f3b4e786328
LeoVFerronato <leo.ferronato@hotmail.com>
2024-09-07 11:29:41 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,26 @@
+window.onload = function() {
+    const storedTotal = localStorage.getItem("total");
+    if (storedTotal) {
+      document.getElementById("total").textContent = storedTotal;
+    }
+    const storedDermatite = localStorage.getItem("dermatite");
+    if (storedDermatite) {
+      document.getElementById("dermatite").textContent = storedDermatite;
+    }
+    const storeDermatose = localStorage.getItem("dermatose");
+    if (storeDermatose) {
+      document.getElementById("dermatose").textContent = storeDermatose;
+    }
+    const storedArtrite = localStorage.getItem("artrite");
+    if (storedArtrite) {
+      document.getElementById("artrite").textContent = storedArtrite;
+    }
+    const storedCalo = localStorage.getItem("calo_de_pata");
+    if (storedCalo) {
+      document.getElementById("calo_de_pata").textContent = storedCalo;
+    }
+}
+
 function atualizar_percentuais() {
     var total = parseInt(document.getElementById("total").textContent);
     var dermatite = parseInt(document.getElementById("dermatite").textContent);
@@ -19,18 +42,22 @@ function atualizar_percentuais() {
 function incrementar() {
     let total = document.getElementById('total');
     total.textContent = parseInt(total.textContent) + 1;
+    localStorage.setItem("total", total.textContent);
     atualizar_percentuais();
 }
 
 function decrementar() {
     let total = document.getElementById('total');
     total.textContent = parseInt(total.textContent) - 1;
+    localStorage.setItem("total", total.textContent);
     atualizar_percentuais();
 }
 function incrementar2() {
     let dermatite = document.getElementById('dermatite');
     dermatite.textContent = parseInt(dermatite.textContent) + 1;
     total.textContent = parseInt(total.textContent) + 1;
+    localStorage.setItem("dermatite", dermatite.textContent);
+    localStorage.setItem("total", total.textContent);
     atualizar_percentuais();
 }
 
@@ -38,12 +65,16 @@ function decrementar2() {
     let dermatite = document.getElementById('dermatite');
     dermatite.textContent = parseInt(dermatite.textContent) - 1;
     total.textContent = parseInt(total.textContent) - 1;
+    localStorage.setItem("dermatite", dermatite.textContent);
+    localStorage.setItem("total", total.textContent);
     atualizar_percentuais();
 }
 function incrementar3() {
     let dermatose = document.getElementById('dermatose');
     dermatose.textContent = parseInt(dermatose.textContent) + 1;
     total.textContent = parseInt(total.textContent) + 1;
+    localStorage.setItem("dermatose", dermatose.textContent);
+    localStorage.setItem("total", total.textContent);
     atualizar_percentuais();
 }
 
@@ -51,28 +82,34 @@ function decrementar3() {
     let dermatose = document.getElementById('dermatose');
     dermatose.textContent = parseInt(dermatose.textContent) - 1;
     total.textContent = parseInt(total.textContent) - 1;
+    localStorage.setItem("dermatose", dermatose.textContent);
+    localStorage.setItem("total", total.textContent);
     atualizar_percentuais();
 }
 function incrementar4() {
     let artrite = document.getElementById('artrite');
     artrite.textContent = parseInt(artrite.textContent) + 1;
+    localStorage.setItem("artrite", artrite.textContent);
     atualizar_percentuais();
 }
 
 function decrementar4() {
     let artrite = document.getElementById('artrite');
     artrite.textContent = parseInt(artrite.textContent) - 1;
+    localStorage.setItem("artrite", artrite.textContent);
     atualizar_percentuais();
 }
 function incrementar5() {
     let calo_de_pata = document.getElementById('calo_de_pata');
     calo_de_pata.textContent = parseInt(calo_de_pata.textContent) + 1;
+    localStorage.setItem("calo_de_pata", calo_de_pata.textContent);
     atualizar_percentuais();
 }
 
 function decrementar5() {
     let calo_de_pata = document.getElementById('calo_de_pata');
     calo_de_pata.textContent = parseInt(calo_de_pata.textContent) - 1;
+    localStorage.setItem("calo_de_pata", calo_de_pata.textContent);
     atualizar_percentuais();
 }
 
@@ -83,4 +120,15 @@ function zerar() {
     calo_de_pata.textContent = 0;
     atualizar_percentuais();
     total.textContent = 0;
+    localStorage.setItem("total", 0);
+    localStorage.setItem("dermatite", 0);
+    localStorage.setItem("dermatose", 0);
+    localStorage.setItem("artrite", 0);
+    localStorage.setItem("calo_de_pata", 0);
 }
+function abrirWhatsApp() {
+    const phone = 5554999669270;
+    const message = "Total avaliado: " + document.getElementById('total');
+    const link = `https://wa.me/${phone}?text=${message}`;
+    window.open(link, '_blank');
+  }
