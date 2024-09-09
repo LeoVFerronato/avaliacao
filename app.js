d604364048bf12df191378716427c2de6f8c9c24
LeoVFerronato <leo.ferronato@hotmail.com>
2024-09-09 19:16:39 -0300

    Update app.js

--- app.js
+++ app.js
@@ -129,7 +129,7 @@ function zerar() {
 }
 function abrirWhatsApp() {
     const phone = 5554999669270;
-    const message = aviarios.value + "\n Total avaliado: " + total.textContent + "\n Dermatite: " + dermatite.textContent + " - " + percentual_dermatite.textContent + "\n Dermatose: " + dermatose.textContent + " - " + percentual_dermatose.textContent + "\n Artrite: " + artrite.textContent + " - " + percentual_artrite.textContent + "\n Calo de pata: " + calo_de_pata.textContent + " - " + percentual_calo_de_pata.textContent;
+    const message = aviarios.value + "\nTotal avaliado: " + total.textContent + "\nDermatite: " + dermatite.textContent + " - " + percentual_dermatite.textContent + "\nDermatose: " + dermatose.textContent + " - " + percentual_dermatose.textContent + "\nArtrite: " + artrite.textContent + " - " + percentual_artrite.textContent + "\nCalo de pata: " + calo_de_pata.textContent + " - " + percentual_calo_de_pata.textContent;
     const link = `https://wa.me/${phone}?text=${encodeURIComponent(message)}`;
     window.open(link, '_blank');
 }
